e5a99c280f4435470ccc28a1f4b7c22274cbb940
jondrews <mistersquiggle@hotmail.com>
2023-09-13 12:27:39 +1000

    Include program description on closure notice

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -31,7 +31,8 @@ export default function Homepage() {
                 Unleash the Power of the Circular Economy: Adapting Your
                 Business for the Future
               </h2>
-              <br /><br />
+              <br />
+              <br />
               <h3>About the program:</h3>
               <p className="intro-copy justify">
                 Introducing the Go Full Circle program, a tailor-made program
@@ -165,7 +166,8 @@ export default function Homepage() {
                   the face of ever-changing requirements.
                 </p>
               </div>
-              <br /><br />
+              <br />
+              <br />
               <p className="intro-copy justify">
                 Join us in reshaping the future of business, as we empower you
                 to embrace the circular economy and propel your company to
@@ -200,15 +202,163 @@ export default function Homepage() {
             <div className="course-description">
               <div className="title-container d-flex">
                 <h1 className="h1 course-title m-auto p-3">
-                  <b>Go Full Circle 2023 is now closed</b>
+                  <b>Go Full Circle waitlist now open</b>
                 </h1>
               </div>
-              <br /><br />
+              <br />
+              <br />
 
               {SHOW_WAITLIST ? (
                 <>
+                  <h2>
+                    Unleash the Power of the Circular Economy: Adapting Your
+                    Business for the Future
+                  </h2>
+                  <br />
+                  <br />
+                  <h3>About the program:</h3>
+                  <p className="intro-copy justify">
+                    Introducing the Go Full Circle program, a tailor-made
+                    program for local businesses to revolutionise their
+                    operations and unlock environmental, social and financial
+                    advantages. <br />
+                    <br />
+                    Embark on an eight-week journey that will equip you with the
+                    knowledge, tools, and network to thrive in the circular
+                    economy. Our immersive virtual course, divided into four
+                    dynamic modules, will kick-start your business's
+                    transformation, to take you beyond just “sustainability”.{" "}
+                    <br />
+                    <br />
+                    Engage with a new community of likeminded people through 4
+                    interactive sessions, where you'll dive deep into the
+                    theories, tools and practical insights of the circular
+                    economy through local, practical real-life case studies.
+                    Then, armed with this newfound wisdom, you'll spend the
+                    following weeks at your own pace applying these concepts to
+                    your business, how to create your own network of support,
+                    with shared peer to peer learning, ensuring you walk away
+                    with a strategy, hands-on learning experience and a
+                    community of like minded businesses to support you into the
+                    future.
+                    <br />
+                    <br />
+                    Each participant will be provided with a one-hour 1 on 1
+                    session with a program facilitator to provide additional
+                    assistance throughout their learning journey. 1 on 1
+                    sessions are available in the second half of the program,
+                    and will be confirmed on an individual basis.
+                  </p>
+                  <br />
+                  <br />
+                  <iframe
+                    className="video-embed"
+                    // width="560"
+                    // height="315"
+                    src="https://www.youtube.com/embed/cFwwLeGW9Qc"
+                    title="YouTube video - Go Full Circle program introduction"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen
+                  ></iframe>
+                  <br />
+                  <br />
+                  <h3>Who is this program for?</h3>
+                  <ul>
+                    <li>
+                      For businesses located in Melbourne, Stonnington or
+                      Boroondara Council Area(s)
+                    </li>
+                    <li>
+                      This program is designed for owners or staff with the
+                      ability to implement new practices in their business
+                    </li>
+                    <li>
+                      It has been designed predominantly for businesses from the
+                      hospitality and retail sectors, however we encourage all
+                      businesses to apply
+                    </li>
+                    <li>
+                      This is designed to be an online experience. Participants
+                      will need to have a computer or tablet with a microphone
+                      and camera that can support Zoom
+                    </li>
+                  </ul>
+                  <br />
+                  <br />
+                  <iframe
+                    className="video-embed"
+                    // width="560"
+                    // height="315"
+                    src="https://www.youtube.com/embed/fyAkRXZRlSk"
+                    title="YouTube video - Introduction to Circular Economy"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen
+                  ></iframe>
+                  <br />
+                  <br />
+                  <h3>Reasons to join this program:</h3>
+                  <div className="reason">
+                    <b className="reason-header">Ignite Innovation</b>
+                    <p className="reason-text">
+                      Discover real and tangible circular opportunities to
+                      create value and practice using the tools needed to
+                      successfully implement them within your business.
+                    </p>
+                  </div>
+                  <div className="reason">
+                    <b className="reason-header">
+                      Collaborative Transformation
+                    </b>
+                    <p className="reason-text">
+                      Collaborate with others and learn how to apply circular
+                      design tools to real business environments, revealing
+                      fresh perspectives and effective approaches to tackling
+                      challenges. Forge connections with like-minded businesses
+                      and local councils, forming a supportive network that
+                      assists your journey towards circularity.
+                    </p>
+                  </div>
+                  <div className="reason">
+                    <b className="reason-header">
+                      Thrive Economically and Improve Patronage
+                    </b>
+                    <p className="reason-text">
+                      Explore where and how you can apply substantial cost
+                      savings through waste reduction and minimisation, or
+                      create new revenue streams, while attracting and retaining
+                      customers who value responsible businesses.
+                    </p>
+                  </div>
+                  <div className="reason">
+                    <b className="reason-header">Attract & Retain Top Talent</b>
+                    <p className="reason-text">
+                      Showcase your dedication to the planet: attract and retain
+                      exceptional staff members who take pride in your improved
+                      mission for the environment.{" "}
+                    </p>
+                  </div>
+                  <div className="reason">
+                    <b className="reason-header">Future-Proof Your Success</b>
+                    <p className="reason-text">
+                      Safeguard your supply chains and your business's
+                      resilience in the face of ever-changing requirements.
+                    </p>
+                  </div>
+                  <br />
+                  <br />
+                  <p className="intro-copy justify">
+                    Join us in reshaping the future of business, as we empower
+                    you to embrace the circular economy and propel your company
+                    to better practices. Apply for the Go Full Circle program
+                    today and unlock boundless possibilities for a brighter,
+                    sustainable tomorrow!
+                  </p>
+                  <br />
+                  <br />
                   <h3>Register your interest in future programs</h3>
-                  <br /><br />
+                  <br />
                   <p className="intro-copy justify">
                     We are no longer accepting enrolments for the Go Full Circle
                     program. If you would like to register your interest in
@@ -224,6 +374,7 @@ export default function Homepage() {
                       Join waitlist
                     </button>
                   </div>
+                  <br />
                   <br />
                   <p className="intro-copy justify">
                     Our team will be in touch when future program details become
@@ -243,15 +394,13 @@ export default function Homepage() {
                   <br />
                   <br />
                   <br />
-                  <p className="intro-copy justify">
-                    The Go Full Circle team.
-                  </p>
+                  <p className="intro-copy justify">The Go Full Circle team.</p>
 
                   <br />
                   <br />
                 </>
               )}
-              <div className="spacer"></div>
+              {/* <div className="spacer"></div> */}
             </div>
           </div>
         </div>
